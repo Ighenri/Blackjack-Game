01f58c438119368b857146bf7a27afb35c6addc0
henry <igwehenry740@gmail.com>
2022-09-13 00:16:43 +0100

    card attributed modified

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let isAlive = true;
 let message =" ";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
-let cardsEl =document.getElementById("card-el");
+let cardsEl =document.getElementById("cards-el");
 
 function startGame() {
     if (sum <= 20  ) {  
@@ -28,3 +28,6 @@ cardsEl.textContent = "cards: " + firstCard + " " + secondCard
 
 }
 
+function newCard() {
+    console.log("drawing a card")
+}
