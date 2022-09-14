90c8b47d2781f33d684d2ccb55ef1977619fd965
henry <igwehenry740@gmail.com>
2022-09-14 21:07:03 +0100

    start function modified

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message =" ";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl =document.getElementById("cards-el");
 
+
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random() * 13) + 1 ;
     if(randomNumber > 10) {
-        return 10;
+        return 10; // this changes any other value greater than 10 and returns it to 10.
     }
     else if ( randomNumber === 1){
         return 11;  // This is the value of he Ace card of the blackjack
@@ -24,7 +24,12 @@ function getRandomCard(){
 
 
 function startGame(){
-    renderGame()
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame();
 } 
 
 function renderGame() {
