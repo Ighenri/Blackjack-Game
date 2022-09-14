e167a57fa7324e087320846dd97557d52805812f
henry <igwehenry740@gmail.com>
2022-09-14 22:25:14 +0100

    randomfunction updated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -8,7 +7,7 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl =document.getElementById("cards-el");
 
-
+console.log (cards)
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random() * 13) + 1 ;
     if(randomNumber > 10) {
