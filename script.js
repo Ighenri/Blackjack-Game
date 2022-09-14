3cf028fa1906815b8b4f743d17564b9d3bff4a97
henry <igwehenry740@gmail.com>
2022-09-14 15:25:01 +0100

    added random function

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-
 let message =" ";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl =document.getElementById("cards-el");
+
+function getRandomCard(){
+    return 5
+}
+
+
 
 function startGame(){
     renderGame()
@@ -37,10 +42,19 @@ sumEl.textContent = "sum: " + sum;
 }
 
 function newCard() {
-    let card = 6;
+    let card = getRandomCard();
     sum += card;
     cards.push(card);
     console.log(cards)
     renderGame();
 }
 
+
+
+function rollDice(){
+    let renderNumber = Math.floor(Math.random() * 6) + 1
+
+   return renderNumber
+
+}
+console.log(rollDice())
